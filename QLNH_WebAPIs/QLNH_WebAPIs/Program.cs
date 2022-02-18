c5906ab85bcd95458f2fc2348b899f4995062f1d
pphan21 <phungquoc.phan@dxc.com>
2022-02-18 11:05:41 +0700

    up

--- QLNH_WebAPIs/QLNH_WebAPIs/Program.cs
+++ QLNH_WebAPIs/QLNH_WebAPIs/Program.cs
@@ -20,7 +20,7 @@ namespace QLNH_WebAPIs
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
